5ac3c0d776e5bb5061e63b60ddd6e0a28fadadfe
Tomergt45 <tomer@yaelsoft.com>
2019-12-21 20:58:31 +0200

    changed google certs

--- webapp/models.py
+++ webapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class PolicyRule(models.Model):
     uid = models.IntegerField(default=0, primary_key=True, unique=True)
     maxAmount = models.FloatField() # Maximum amount in stoshis
-    destinations = models.TextField() # Destinations seperated by delimiter
+    destinations = models.TextField() # Destinations - comma separated
 
 class Transaction(models.Model):
     amount = models.FloatField() # Sent amount in stoshis
